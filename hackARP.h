4994dd91b8b43e57db944da18dfc0691f9e1545f
2gilhee <2gilhee@naver.com>
2020-11-22 01:15:04 -0800

    hackARP header file.

---
+++ hackARP.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <typeinfo>
+#include <bitset>
+#include <iomanip>
+#include <string.h>
+#include <unistd.h> // for sleep() function
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <pcap.h>
+#include <netinet/ip.h>
+#include <net/ethernet.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/ether.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <net/if.h>
+
+void atoiIP(char* addr, uint8_t* ip);
+void printMAC(uint8_t* add, int length);
+int getAttackerMAC(uint8_t* attacker_mac, char* device);
+void getVictimMAC(uint8_t* victim_ip, uint8_t* victim_mac);
+void makeEther(struct ether_header* ether, uint8_t* attacker_mac, uint8_t* victim_mac);
+void makeArp(struct ether_arp* arp, uint8_t* server_ip, uint8_t* victim_ip, uint8_t* attacker_mac, uint8_t* victim_mac);
